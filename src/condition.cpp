a12f893d753b42c6301db7ae025170d2354c2cef
Ekaterina Ledeneva <cthfvrl@gmail.com>
2020-05-03 23:58:25 +0300

    Indentation in default condition

--- src/condition.cpp
+++ src/condition.cpp
@@ -1,8 +1,9 @@
 #include "condition.hpp"
 #include <iostream>
+#include <string>
 
 void Condition::print_condition(size_t indent, bool reverse) {
-    std::cout << (reverse ? "" : "!") << "[catch {";
+    std::cout << std::string(indent, '\t') << (reverse ? "" : "!") << "[catch {";
     print();
     std::cout << "}]";
 }
